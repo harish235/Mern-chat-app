a332575aac78bc12ec93a2352d586f19dc86ccf6
Harish <harishsundar235@gmail.com>
2021-05-08 12:31:05 +0530

    first commit

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaComments } from "react-icons/fa";
+
+function LandingPage() {
+    return (
+        <>
+        <div className="app">
+            <FaComments style={{ fontSize: '4rem' }} /><br />
+            <span style={{ fontSize: '2rem' }}>Let's Start Chating!</span>
+        </div>
+        </>
+    )
+}
+
+export default LandingPage
